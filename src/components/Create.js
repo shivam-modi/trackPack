fca204680926c00851c37a6650c5467a7c0c77be
keshav25201 <keshav25201@gmail.com>
2021-05-30 09:11:59 +0530

    minor

--- src/components/Create.js
+++ src/components/Create.js
@@ -15,64 +15,68 @@ const Create = (props) => {
   };
   return (
     <>
-    <div className="create container" style={{ padding: "50px" }}>
-      <h1>Create Page</h1>
-      <form className="createform">
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">
-            Title
-          </label>
-          <input
-            type="text"
-            class="form-control"
-            id="exampleInputEmail1"
-            value={input.title}
-            onChange={(e) => setInput({ ...input, title: e.target.value })}
-          />
-        </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">
-            description
-          </label>
-          <input
-            type="text"
-            class="form-control"
-            id="exampleInputPassword1"
-            value={input.desc}
-            onChange={(e) => setInput({ ...input, desc: e.target.value })}
-          />
-        </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">
-            manufacturer
-          </label>
-          <input
-            type="text"
-            class="form-control"
-            id="exampleInputPassword1"
-            value={input.manf}
-            onChange={(e) => setInput({ ...input, manf: e.target.value })}
-          />
-        </div>
-        <button type="submit" class="btn btn-primary" onClick={handleSubmit}>
-          Submit
-        </button>
-      </form>
-    </div>
-    <div class="area" >
-            <ul class="circles">
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-            </ul>
-    </div >
+      <div className="create container" style={{ padding: "50px" }}>
+        <h1>Create Page</h1>
+        <form className="createform">
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">
+              Title
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInputEmail1"
+              value={input.title}
+              onChange={(e) => setInput({ ...input, title: e.target.value })}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              description
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInputPassword1"
+              value={input.desc}
+              onChange={(e) => setInput({ ...input, desc: e.target.value })}
+            />
+          </div>
+          <div class="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              manufacturer
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInputPassword1"
+              value={input.manf}
+              onChange={(e) => setInput({ ...input, manf: e.target.value })}
+            />
+          </div>
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={handleSubmit}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+      <div className="area">
+        <ul className="circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
     </>
   );
 };
